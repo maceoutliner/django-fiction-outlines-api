3bb2cc23ea420fe4bd988b760eb3770a0335ac30
Daniel Andrlik <daniel@andrlik.org>
2022-03-17 16:22:42 -0400

    Update for Django-fiction-outlines 0.4.0 (#20)
    * Switch to poetry for dependency management.
    
    * Remove circleci job
    
    * Update version number and changelog
    
    * Update for Django >=3.2, and switch to the maintained dj-taggit-serializer
    
    Fixes #17. Fixes #18.
    
    * Switch to github actions from circleci and pytest.
    
    Fixes #19
    
    * Add developement branch to github actions for testing.

--- fiction_outlines_api/__init__.py
+++ fiction_outlines_api/__init__.py
@@ -1,2 +1,1 @@
-__version__ = '0.2.1'
-default_app_config = 'fiction_outlines_api.apps.FictionOutlinesApiConfig'
+__version__ = '0.3.0'
--- fiction_outlines_api/exceptions.py
+++ fiction_outlines_api/exceptions.py
@@ -1,5 +1,5 @@
 from rest_framework.exceptions import APIException
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class TreeUnavailable(APIException):
